cb7f0e1a576cdf2f29488d7fc2c1f65a70478bca
Mezhik1337 <59055036+Mezhik1337@users.noreply.github.com>
2021-10-22 12:59:03 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace Excel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.AddRowButton = new System.Windows.Forms.Button();
+            this.DeleteRowButton = new System.Windows.Forms.Button();
+            this.AddColumnButton = new System.Windows.Forms.Button();
+            this.DeleteColumnButton = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(0, 0);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(1023, 605);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.DeleteColumnButton);
+            this.panel1.Controls.Add(this.AddColumnButton);
+            this.panel1.Controls.Add(this.DeleteRowButton);
+            this.panel1.Controls.Add(this.AddRowButton);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1023, 112);
+            this.panel1.TabIndex = 1;
+            // 
+            // AddRowButton
+            // 
+            this.AddRowButton.Location = new System.Drawing.Point(346, 42);
+            this.AddRowButton.Name = "AddRowButton";
+            this.AddRowButton.Size = new System.Drawing.Size(106, 31);
+            this.AddRowButton.TabIndex = 0;
+            this.AddRowButton.Text = "Add Row";
+            this.AddRowButton.UseVisualStyleBackColor = true;
+            // 
+            // DeleteRowButton
+            // 
+            this.DeleteRowButton.Location = new System.Drawing.Point(479, 41);
+            this.DeleteRowButton.Name = "DeleteRowButton";
+            this.DeleteRowButton.Size = new System.Drawing.Size(106, 31);
+            this.DeleteRowButton.TabIndex = 1;
+            this.DeleteRowButton.Text = "Delete Row";
+            this.DeleteRowButton.UseVisualStyleBackColor = true;
+            // 
+            // AddColumnButton
+            // 
+            this.AddColumnButton.Location = new System.Drawing.Point(606, 41);
+            this.AddColumnButton.Name = "AddColumnButton";
+            this.AddColumnButton.Size = new System.Drawing.Size(106, 31);
+            this.AddColumnButton.TabIndex = 2;
+            this.AddColumnButton.Text = "Add Column";
+            this.AddColumnButton.UseVisualStyleBackColor = true;
+            // 
+            // DeleteColumnButton
+            // 
+            this.DeleteColumnButton.Location = new System.Drawing.Point(744, 41);
+            this.DeleteColumnButton.Name = "DeleteColumnButton";
+            this.DeleteColumnButton.Size = new System.Drawing.Size(140, 31);
+            this.DeleteColumnButton.TabIndex = 3;
+            this.DeleteColumnButton.Text = "Delete Column";
+            this.DeleteColumnButton.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(29, 50);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(288, 26);
+            this.textBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1023, 605);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Excel_by_Mezhybovskiy";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button DeleteColumnButton;
+        private System.Windows.Forms.Button AddColumnButton;
+        private System.Windows.Forms.Button DeleteRowButton;
+        private System.Windows.Forms.Button AddRowButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Excel
+{
+    public partial class Form1 : Form
+    {
+        const int Measures = 200;
+        int rowNum = 0;
+        int columnNum = 0;
+        public Dictionary<string, MyCell> dictionary = new Dictionary<string, MyCell>();
+        public Form1()
+        {
+            InitializeComponent();
+            OpenFileDialog1.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+            SaveFileDialog1.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+            CreateDataGridView(Measures, Measures);
+            for (int i = 0; i < rowNum; ++i)
+            {
+                for (int j; j < columnNum; ++j)
+                {
+                    string cellName = setCellName(j, i);
+                    MyCell cell = new MyCell(cellName);
+                    cell.Value = 0;
+                    cell.exp = "0";
+                    cell.column = j;
+                    cell.row = i;
+                    dictionary.Add(CellName, cell);
+                }
+            }
+            this.ActivateControl = dgv;
+        }
+    }
+
+    public class MyCell : DataGridViewTextBoxCell
+    {
+        double val;
+        string name;
+        string exp;
+        private int row;
+        private int column;
+        private List<string> cellsIDependOn = new List<string>();
+        private List<string> cellsDepentOnMe = new List<string>();
+
+        public MyCell()
+        {
+
+        }
+
+        public MyCell(string n) 
+        {
+
+        }
+        public string Name;
+        public string Exp;
+        public double Value;
+        public int Row;
+        public int Column;
+        public List<string> IDependOn;
+        public List<string> DepentOnMe;
+
+    }
+    private void CreateDataGidView(int columns, int rows) 
+    {
+        for (int i = 0; i < columns; ++i) 
+        {
+            DataGridViewColumn A = new DataGridViewColumn();
+            A.HeaderText = SetColumnName();
+            A.Name = A.HeaderText;
+            MyCell cellA = new MyCell();
+            A.CellTemplate = cellA;
+            dgv.Columns.Add(A);
+            ++columnNum;
+        }
+    }
+        
+}
+
+
+
+
